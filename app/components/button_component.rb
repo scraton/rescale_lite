e7da661d31d9ee56a36256d431b52b7f55b4f1d1
Stephen Craton <stephen@craton.dev>
2024-05-15 17:55:59 +0000

    Fix padding around button within table rows

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -4,6 +4,6 @@ class ButtonComponent < ViewComponent::Base
   def initialize(label:, url: nil)
     @label = label
     @url = url
-    @classnames = 'font-medium text-center text-white bg-blue-600 rounded hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 px-5 py-3 text-base text-nowrap'
+    @classnames = 'inline-block font-medium text-center text-white bg-blue-600 rounded hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 px-5 py-3 text-base text-nowrap'
   end
 end
